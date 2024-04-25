7b75f0eb5a86fb27ec55fc1185b8b1f0aeed1dad
DimAzin <97674115+DimAzin@users.noreply.github.com>
2024-04-25 10:15:18 +0300

    Add files via upload

---
+++ homeworks2.py
@@ -0,0 +1,8 @@
+name = "Dmitrij"
+print("Name: " + name)
+age = 58
+print("Age: " + str(age))
+age = age + 1
+print("New Age: "+ str(age))
+is_student = True
+print("Is Student: " + str(is_student))
