79b05e47a512aebd1f1d7843d673afd39a867cc5
DimAzin <97674115+DimAzin@users.noreply.github.com>
2024-04-27 18:14:33 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,14 @@
+my_list = ['apple', 'cocos', 'banan', 'berry', 'chery']
+print("My list: ", my_list)
+print("First element: ", my_list[0])
+print("Last element: ", my_list[-1])
+print("Elements from 3 to 5: ", my_list[2:5])
+my_list[2] = 'potato'
+print("Modified list: ", my_list)
+
+my_dict = {'apple': 123, 'cocos': 324, 'banan': 8686, 'berry': 753957, 'chery': 7593}
+print("My dictionary: ", my_dict)
+print("Value - apple: ", my_dict['apple'])
+my_dict['cocos'] = 1111
+my_dict['rasberry'] = 8492387594
+print("Modified dictionary: ", my_dict)
