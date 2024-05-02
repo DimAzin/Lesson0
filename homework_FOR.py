03306014e32e4ef35b112de3a607383876b84f18
DimAzin <97674115+DimAzin@users.noreply.github.com>
2024-05-02 20:11:55 +0300

    Add files via upload

---
+++ homework_FOR.py
@@ -0,0 +1,11 @@
+list_cars = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+
+for i in list_cars:
+    print('Я езжу на автомобиле марки ', i)
+
+cars_count = 0
+for i in list_cars:
+    print('Я езжу на автомобиле марки ', i)
+    cars_count += 10
+
+print(cars_count)
