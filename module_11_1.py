9642ccbbced75e0e0385050b0f2b5ca131ea72d2
DimAzin <97674115+DimAzin@users.noreply.github.com>
2024-07-28 17:59:19 +0300

    Add files via upload

--- module_11_1.py
+++ module_11_1.py
@@ -1,4 +1,4 @@
-import requests 
+import requests
 
 r = requests.get('https://google.com/')
 print(r.text)
@@ -12,6 +12,11 @@ print(r)
 print(r.status_code)
 
 r = requests.get('https://api.github.com', auth=('user', 'pass'))
-
 print(r.status_code)
 print(r.headers['content-type'])
+
+r = requests.get('https://api.github.com/events')
+r.json()
+print(r.status_code)
+print(r.json())
+
