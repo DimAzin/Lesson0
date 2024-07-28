a6ea715259721cedf6a2b4a32fedb4176edcec3b
DimAzin <97674115+DimAzin@users.noreply.github.com>
2024-07-28 17:47:21 +0300

    Add files via upload

---
+++ module_11_1.py
@@ -0,0 +1,17 @@
+import requests 
+
+r = requests.get('https://google.com/')
+print(r.text)
+print(r.status_code)
+
+url = requests.Request(None, 'https://google.com/?', params={'Data1': 'data'}).prepare().url
+print(url)
+
+r = requests.get("https://en.wikipedia.org/wiki/Python")
+print(r)
+print(r.status_code)
+
+r = requests.get('https://api.github.com', auth=('user', 'pass'))
+
+print(r.status_code)
+print(r.headers['content-type'])
